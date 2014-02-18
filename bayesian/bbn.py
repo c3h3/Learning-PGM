5bf91a4e5cd9557002ba2c0f31e0d7c4e75bd258
Liquid <sk413025@gmail.com>
2014-02-18 21:53:03 +0800

    add Node class

--- bayesian/bbn.py
+++ bayesian/bbn.py
@@ -1,4 +1,20 @@
 
+class Node(object):
+
+    def __init__(self, name, parents=[], children=[]):       
+        self.name = name
+        self.parents = parents[:]
+        self.children = children[:]
+	
+		
+class BBNNode(Node):
+
+    def __init__(self, factor):
+        super(BBNNode, self).__init__(factor.__name__)
+        self.func = factor
+        self.argspec = get_args(factor)
+         
+            
 
 def build_bbn(*args, **kwds):
     '''Builds a BBN Graph from
@@ -8,11 +24,15 @@ def build_bbn(*args, **kwds):
     name = kwds.get('name')
     variable_nodes = dict()
     factor_nodes = dict()
-
-
+	
     for factor in args:
         factor_args = get_args(factor)
         variables.update(factor_args)
         bbn_node = BBNNode(factor)
         factor_nodes[factor.__name__] = bbn_node
+	
+	
+	
+
+
 
